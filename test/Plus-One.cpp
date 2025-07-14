710fef3c76376c571ffdb206ea2c8137d27cca16
Ahmed Fawzy <138530280+AhmedFawzy10@users.noreply.github.com>
2025-07-14 12:51:47 +0300

    [66] [Time Beats: 100.00%] [Memory Beats: 65.92%] - LeetPush

--- test/Plus-One.cpp
+++ test/Plus-One.cpp
@@ -2,20 +2,7 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        if(digits[n-1] != 9)
-        {
-            digits[n-1]++;
-            return digits;
-        }
-        if(n == 1)
-        {
-            digits.resize(2);
-            digits[0] = 1, digits[1] = 0;
-            return digits;
-        }
-        digits[n-1] = 0;
-        cout<<"How";
-        for(int i = n-2; i >= 0; i --)
+        for(int i = n-1; i >= 0; i --)
         {
             if(digits[i] != 9)
             {
@@ -24,7 +11,6 @@ public:
             }
             digits[i] = 0;
         }
-        cout<<"How";
         digits.push_back(0);
         digits[0] = 1;
         return digits;
