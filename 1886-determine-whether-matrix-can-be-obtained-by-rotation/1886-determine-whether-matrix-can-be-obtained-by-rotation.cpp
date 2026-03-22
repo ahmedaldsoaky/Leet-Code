2b7f7ee881148ee46259f466a45e84fa3e88f474
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-03-23 01:31:20 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,77 +1,31 @@
 class Solution {
 public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        if(mat == target) return true;
         int n = mat.size();
-        try2(mat, target, n);
-        try3(mat, target, n);
-        try4(mat, target, n);
-        if(try1(mat, target, n))// same
-            return true;
-        if(try2(mat, target, n))// 90
-            return true;
-        if(try3(mat, target, n))// 180
-            return true;
-        if(try4(mat, target, n))// 270
-            return true;
+        for(int i = 0; i < 4; i++)
+        {
+            rotate(mat, n);
+            if(mat == target)
+                return true;
+        }
         return false;
     }
-
-    bool try1(vector<vector<int>>& mat, vector<vector<int>>& target, int n)
+    void rotate(vector<vector<int>>& mat, int n)
     {
         for(int i = 0; i < n; i++)
+            for(int j = i; j < n; j++)
+                swap(mat[i][j], mat[j][i]);
+        for(int i = 0; i < n; i++)
+            reverse(mat[i].begin(), mat[i].end());
+        for(int i = 0; i < n; i++)
+        {
             for(int j = 0; j < n; j++)
-                if(mat[i][j] != target[i][j])
-                    return false;
-        return true;
-    }
-
-    bool try2(vector<vector<int>>& mat, vector<vector<int>>& target, int n)
-    {
-        for(int i = 0, l = 0; i < n; i++, l++)// column
-        {
-            for(int j = n-1, m = 0; j >= 0; j--, m++)// row
-            {
-                cout<<mat[j][i]<<' ';
-                if(mat[j][i] != target[l][m])
-                    return false;
-            }
-            cout<<endl;
-        }
-        cout<<"\n -------------- 1 ---------------- \n\n";
-        return true;
-    }
-
-    bool try3(vector<vector<int>>& mat, vector<vector<int>>& target, int n)// 180
-    {
-        for(int i = n-1, l = 0; i >= 0; i--, l++)// row
-        {
-            for(int j = n-1, m = 0; j >= 0; j--, m++)// column
             {
                 cout<<mat[i][j]<<' ';
-                if(mat[i][j] != target[l][m])
-                    return false;
-            }
+            }   
             cout<<endl;
         }
-        cout<<"\n -------------- 2 ---------------- \n\n";
-        return true;
+        cout<<"\n------------\n";
     }
-
-    bool try4(vector<vector<int>>& mat, vector<vector<int>>& target, int n)
-    {
-        for(int i = n-1, l = 0; i >= 0; l++, i--)// column
-        {
-            for(int j = 0, m = 0; j < n; j++, m++)// row
-            {
-                cout<<mat[j][i]<<' ';
-                if(mat[j][i] != target[l][m])
-                    return false;
-            }
-            cout<<endl;
-        }
-        cout<<"\n -------------- 3 ---------------- \n\n";
-        return true;
-    }
-
-    
 };
