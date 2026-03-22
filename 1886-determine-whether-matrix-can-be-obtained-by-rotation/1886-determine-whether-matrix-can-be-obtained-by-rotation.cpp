915e95cd2ab4fb5e9021bde12c0cef5f945a3bdd
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-03-23 01:50:39 +0200

    Time: 0 ms (100%), Space: 14.6 MB (41.7%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -2,28 +2,19 @@ class Solution {
 public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
         int n = mat.size();
-        for(int i = 0; i < 4; i++)
-        {
-            if(mat == target) return true;
-            rotate(mat, n);
-        }
-        return false;
-    }
-    void rotate(vector<vector<int>>& mat, int n)
-    {
-        for(int i = 0; i < n; i++)
-            for(int j = i; j < n; j++)
-                swap(mat[i][j], mat[j][i]);
-        for(int i = 0; i < n; i++)
-            reverse(mat[i].begin(), mat[i].end());
+        int mask = 0b1111;
         for(int i = 0; i < n; i++)
         {
             for(int j = 0; j < n; j++)
             {
-                cout<<mat[i][j]<<' ';
-            }   
-            cout<<endl;
+                if(mat[i][j] != target[i][j]) mask &= 0b1110;
+                if (mat[i][j] != target[j][n - 1 - i]) mask &= 0b1101;
+                if (mat[i][j] != target[n - 1 - i][n - 1 - j]) mask &= 0b1011;
+                if (mat[i][j] != target[n - 1 - j][i]) mask &= 0b0111;
+                if (mask == 0) return false;
+            }
         }
-        cout<<"\n------------\n";
+        return mask;
     }
+    
 };
