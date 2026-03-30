75bcedda5e345489666ffe402656c48ddc22f0e3
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-03-30 06:45:08 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -5,15 +5,11 @@ public:
         int freqe[26] = {}, freqo[26] = {};
         for(int i = 0; i < n; i++)
             if(i & 1)
-                freqo[s1[i] - 'a']++;
+                freqo[s1[i] - 'a']++, freqo[s2[i] - 'a']--;
             else
-                freqe[s1[i] - 'a']++;
+                freqe[s1[i] - 'a']++, freqe[s2[i] - 'a']--;
         for(int i = 0; i < n; i++)
-            if((i & 1) && freqo[s2[i] - 'a'])
-                freqo[s2[i] - 'a']--;
-            else if(!(i & 1) && freqe[s2[i] - 'a'])
-                freqe[s2[i] - 'a']--;
-            else
+            if(freqo[s1[i] - 'a'] || freqe[s1[i] - 'a'])
                 return false;
         return true;
     }
