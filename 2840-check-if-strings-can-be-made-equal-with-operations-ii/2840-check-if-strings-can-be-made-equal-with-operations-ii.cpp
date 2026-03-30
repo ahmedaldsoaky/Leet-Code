fcd6da2b42989db582b3ce35a8443b5a527551cb
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-03-30 06:31:38 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,27 +2,19 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
         int n = s1.size();
-        map<int, int> mpe, mpo;
+        int freqe[26] = {}, freqo[26] = {};
         for(int i = 0; i < n; i++)
             if(i & 1)
-                mpo[s1[i]]++;
+                freqo[s1[i] - 'a']++;
             else
-                mpe[s1[i]]++;
+                freqe[s1[i] - 'a']++;
         for(int i = 0; i < n; i++)
-        {
-            if(i & 1) {
-                if(mpo[s2[i]] > 0)
-                    mpo[s2[i]]--;
-                else
-                    return false;
-            }
-            else {
-                if(mpe[s2[i]] > 0)
-                    mpe[s2[i]]--;
-                else
-                    return false;
-            }
-        }
+            if((i & 1) && freqo[s2[i] - 'a'])
+                freqo[s2[i] - 'a']--;
+            else if(!(i & 1) && freqe[s2[i] - 'a'])
+                freqe[s2[i] - 'a']--;
+            else
+                return false;
         return true;
     }
 };
