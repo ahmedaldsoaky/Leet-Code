0ab71b4718e95d4f5bea6a098165c9d98185d316
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-04-20 14:37:44 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        map<int, int> mp;
-        for(auto c : s)mp[c]++;
         int n = s.size(), mx = 0;
         string ans = "";
         for(int i = 0; i < n; i ++)
         {
             int l = i-1, r = i+1;
-            while(r < n && s[r] == s[i])r++;
-            while(l >= 0 && s[l] == s[i])l--;
+            while(r < n && s[r] == s[i]) r++;
+            while(l >= 0 && s[l] == s[i]) l--;
             while(l >= 0 && r < n && s[l] == s[r])
                 l--, r++;
             if(mx < r - l + 1)
@@ -19,15 +17,5 @@ public:
             }
         }
         return ans;
-    }
-    bool is_palindrom(string s)
-    {
-        int i = 0, j = s.size()-1;
-        while(i < j)
-        {
-            if(s[i] != s[j]) return false;
-            i++, j--;
-        }
-        return true;
     }
 };
