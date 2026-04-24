30ec2c262c6a3f128a219ab36d9a5c36fd4bd12f
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-04-24 18:17:35 +0300

    Time: 0 ms (100%), Space: 8.9 MB (90.95%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int l = count(moves.begin(), moves.end(), 'L');
+        int r = count(moves.begin(), moves.end(), 'R');
+        int _ = count(moves.begin(), moves.end(), '_');
+        return _ + abs(l - r);
+    }
+};
