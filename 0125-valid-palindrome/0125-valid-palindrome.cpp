8f0907ea73baa3203cbf9c4138d125eff143c19c
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-07-07 11:14:42 +0300

    Time: 4 ms (14.66%), Space: 10.3 MB (15.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -6,8 +6,10 @@ public:
             if(isalnum(c))
                 m += tolower(c);
         cout<<m<<endl;
-        for(int i = 0; i < m.size()/2; i++)
-            if(m[i] != m[m.size()-1-i]) return false;
+        int i = 0, j = m.size()-1;
+        while(i < j)
+            if(m[i] != m[j]) return false;
+            else i++, j--;
         return true;
     }
 };
