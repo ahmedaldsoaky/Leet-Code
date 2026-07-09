fcd6967944619e7e294c1b089fda117bc53353fc
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-07-09 18:04:30 +0300

    Time: 1452 ms (5.01%), Space: 297.5 MB (5.48%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        int n = arr.size();
+        set<vector<int>> st;
+        vector<vector<int>> ans;
+        sort(arr.begin(), arr.end());
+        for(int i = 0; i < n; i++)
+        {
+            int t = arr[i];
+            int l = i+1, r = n-1;
+            while(l < r)
+            {
+                if(l == i || r == i)continue;
+                int sum = arr[l] + arr[r];
+                if(sum == -t)
+                {
+                    vector<int> a = {arr[l], arr[r], t};
+                    sort(a.begin(), a.end());
+                    st.insert(a);
+                    l++, r--;
+                }
+                else if(sum < -t)
+                    l++;
+                else
+                    r--;
+            }
+        }
+        for(auto a : st) ans.push_back(a);
+        return ans;
+    }
+};
