60d638eb19c1ba9f7c42e4889d0d8a8f9ff2cace
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-07-09 19:00:24 +0300

    Time: 4 ms (21.21%), Space: 43.8 MB (5.09%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int c0 = count(nums.begin(), nums.end(), 0);
-        if(c0 > 1)
-        {
-            vector<int> ans(nums.size(), 0);
-            return ans;
-        }
-        else if(c0 == 1)
-        {
-            int p = 1, idx = 0;
-            vector<int> ans(nums.size(), 0);
-            for(int i = 0; i < nums.size(); i++)
-                if(nums[i])
-                    p*=nums[i];
-                else
-                    idx = i;                    
-            cout<<p;
-            ans[idx] = p;
-            return ans;
-        }
-        int p = 1;
-        for(int& i : nums)p*=i;
-        vector<int> ans;
-        for(int& i : nums)ans.push_back(p/i);
+        int n = nums.size();
+        vector<int> ans, pref(n+1, 1), suff(n+2, 1);
+        for(int i = 0; i < n; i++)
+            pref[i+1] = pref[i] * nums[i];
+        for(int i = n-1; i >= 0; i--)
+            suff[i+1] = suff[i+2] * nums[i];
+        for(int i = 0; i < n; i++)
+            if(!i) ans.push_back(suff[i+2]);
+            else if(i == n-1) ans.push_back(pref[i]);
+            else ans.push_back(pref[i] * suff[i+2]);
         return ans;
     }
 };
