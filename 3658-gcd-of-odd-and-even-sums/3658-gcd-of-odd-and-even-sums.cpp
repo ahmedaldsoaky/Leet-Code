a47d7594ad2c9487b30b5ffd7fe6fc5172e75bd6
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-07-15 04:16:43 +0300

    Time: 7 ms (11.74%), Space: 8.4 MB (85.02%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumOdd = n*(2*n+1);
         int sumEven = n*(n+1);
+        int sumOdd = n * n;
+        cout<<sumOdd<<' '<<sumEven<<endl;
         return gcd(sumEven, sumOdd);
     }
 };
