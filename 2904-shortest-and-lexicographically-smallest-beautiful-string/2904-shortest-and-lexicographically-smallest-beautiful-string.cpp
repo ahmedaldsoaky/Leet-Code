15ccd33d48ad1586357470d88e45a218f5e359c0
Ahmed Aldsoaky <138530280+ahmedaldsoaky@users.noreply.github.com>
2026-08-26 23:29:15 +0300

    Time: 2 ms (34.78%), Space: 8.7 MB (64.71%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        deque<char> dq;
-        int c = 0, n = s.size();
-        string res = "";
-        for(char &ch : s)
+        if (ranges::count(s, '1') < k) {
+            return "";
+        }
+
+        string ans = s;
+        int c = 0;
+        int l = 0, r = 0, n = s.size();
+        while(r < n)
         {
-            dq.push_back(ch);
-            if(ch == '1')c++;
-            while(c > k || dq.front() == '0')
+            c += s[r]-'0';
+            while(c > k || s[l] == '0')
             {
-                if(dq.front() == '1') c--;
-                dq.pop_front();
+                if(s[l] == '1') c--;
+                l++;
             }
             if(c == k)
             {
-                string m = "";
-                for(char& c : dq) m += c;
-                if(!res.size() || m.size() < res.size() || (m.size() == res.size() && m < res))
-                    res = m;
+                string m = s.substr(l, r - l + 1);
+                if(m.size() < ans.size() || (m.size() == ans.size() && m < ans))
+                    ans = m;
             }
+            r++;
         }
-        return res;
+        return ans;
+        // deque<char> dq;
+        // int c = 0;
+        // string res = "";
+        // for(char &ch : s)
+        // {
+        //     dq.push_back(ch);
+        //     if(ch == '1')c++;
+        //     while(c > k || dq.front() == '0')
+        //     {
+        //         if(dq.front() == '1') c--;
+        //         dq.pop_front();
+        //     }
+        //     if(c == k)
+        //     {
+        //         string m(dq.begin(), dq.end());
+        //         if(!res.size() || m.size() < res.size() || (m.size() == res.size() && m < res))
+        //             res = m;
+        //     }
+        // }
+        // return res;
     }
 };
